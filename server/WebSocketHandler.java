a7de85355fe8c34fdd3b93b118176cc6af5c32cc
Ostyn Sy <osy304@insite.4cd.edu>
2022-10-20 19:09:45 -0700

    added nav bar

---
+++ server/WebSocketHandler.java
@@ -0,0 +1,9 @@
+import java.net.ServerSocket;
+
+public class WebSocketHandler{
+
+    public static void main(String[] args) throws IOException{
+
+    }
+
+}
---
+++ website/src/Navbar.js
@@ -0,0 +1,30 @@
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import './css/Navbar.css'
+
+export default function Navbar() {
+  return (
+    <header>
+      <Link id="logo" href="#">DashKo</Link>
+      <nav>
+        <ul>
+          <li><CustomLink to="/" id="current">Home</CustomLink></li>
+          <li><CustomLink to="/about">About</CustomLink></li>
+        </ul>
+      </nav>
+</header>
+
+  )
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
+}
